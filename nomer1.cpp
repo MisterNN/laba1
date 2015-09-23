b67440717f739a260ffca105656541f13cec8690
MisterNN <mr.cnas@mail.ru>
2015-09-23 21:18:24 +0300

    2nd vers

--- nomer1.cpp
+++ nomer1.cpp
@@ -6,16 +6,16 @@ long double factorial (double z)
  if( z == 0 ) return 1;
  return z * factorial (z - 1);
 }
-void sinus(double x,double epsilon)
+double sinus(double x,double epsilon)
 {   
 	double summa=0.0,y=-1.0;
 	int r=0;
     if (x==0)
 	{cout << " 0 \n";
-	return;}
+	return 0;}
 	if (epsilon>=1){
 		cout << "Slishkom bol'shoi epsilon \n";
-	return;}
+	return 0;}
 	for (double n=0.0;n<n+1;n++){
 	double z =2*n+1;
 	if (abs((pow(y,n)*pow(x,2*n+1))/factorial(z))>epsilon){
@@ -25,11 +25,10 @@ void sinus(double x,double epsilon)
 	   epsilon*=10;
 	   r++;}
    cout.precision(r+1);
-cout << fixed << summa << endl;
-return;}
+return summa;}
 int main(){
    double x,epsilon;
    cin >> x >> epsilon;
-   sinus(x,epsilon);
+   cout << sinus(x,epsilon) << endl;
    return 0;
 }
